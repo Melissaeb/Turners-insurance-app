d883b98d31821caa282a56b44603b0bdd01d4235
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-06-01 20:27:15 +1200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import VehicleRecognition from "./components/VehicleRecognition/VehicleRecognition";
 import Footer from "./components/Footer/Footer";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <VehicleRecognition />
+      <Routes>
+        {/* Redirects to AutoDetect Vehicle recognition from / */}
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to={"/Cars/finance-insurance/car-insurance/autodetect/"}
+            />
+          }
+        />
+        <Route
+          path="/:section/finance-insurance/car-insurance/autodetect/"
+          element={<VehicleRecognition />}
+        />
+      </Routes>
       <Footer />
     </div>
   );

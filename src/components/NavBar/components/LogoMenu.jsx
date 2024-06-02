e564ab5fcdde475e76b0923a0b870968b6722705
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-06-02 15:10:50 +1200

    RWD added

--- src/components/NavBar/components/LogoMenu.jsx
+++ src/components/NavBar/components/LogoMenu.jsx
@@ -4,6 +4,8 @@ import turnersCarLogo from "/turnerscars-logo.png";
 import personIcon from "../../../assets/person.svg";
 import phoneIcon from "../../../assets/phone.svg";
 import locationIcon from "../../../assets/location.svg";
+import searchIcon from "../../../assets/search.svg";
+import hamburgerIcon from "../../../assets/hamburger.svg";
 
 export default function LogoMenu() {
   return (
@@ -24,23 +26,17 @@ export default function LogoMenu() {
               href="https://www.turners.co.nz/Login/"
               className={`${styles.loginLink} ${styles.menuLink}`}
             >
-              <img
-                src={personIcon}
-                alt="Person Icon"
-                className={styles.personIcon}
-              />
-              LOGIN <span className={styles.or}>OR </span>REGISTER
+              <img src={personIcon} alt="Person Icon" className={styles.icon} />
+              <span className={styles.buttonText}>
+                LOGIN <span className={styles.or}>OR </span>REGISTER
+              </span>
             </a>
           </li>
 
           <li className={styles.menuItem}>
             <a href="tel:0800887637" className={styles.menuLink}>
-              <img
-                src={phoneIcon}
-                alt="Phone icon"
-                className={styles.phoneIcon}
-              />
-              0800 887 637
+              <img src={phoneIcon} alt="Phone icon" className={styles.icon} />
+              <span className={styles.buttonText}>0800 887 637</span>
             </a>
           </li>
 
@@ -49,12 +45,31 @@ export default function LogoMenu() {
               href="https://www.turners.co.nz/Company/Branches/"
               className={styles.menuLink}
             >
+              <img src={searchIcon} alt="Search icon" className={styles.icon} />
+            </a>
+          </li>
+
+          <li className={styles.menuItem}>
+            <a className={styles.menuLink}>
+              <img
+                src={hamburgerIcon}
+                alt="Menu icon"
+                className={styles.icon}
+              />
+            </a>
+          </li>
+
+          <li className={styles.menuItem}>
+            <a
+              href="https://www.turners.co.nz/Cars/Used-Cars-for-Sale/"
+              className={styles.menuLink}
+            >
               <img
                 src={locationIcon}
                 alt="Location icon"
-                className={styles.locationIcon}
+                className={styles.icon}
               />
-              Find Us
+              <span className={styles.buttonText}>Find Us</span>
             </a>
           </li>
 
@@ -63,7 +78,7 @@ export default function LogoMenu() {
               href="https://www.turners.co.nz/Cars/Special-Events-Cars/chinese-speaking-contacts/"
               className={`${styles.chineseLink} ${styles.menuLink}`}
             >
-              中文
+              <span className={styles.buttonText}>中文</span>
             </a>
           </li>
         </ul>
